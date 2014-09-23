e5c94849c8c7bcd95ba55820f3d4e9f0b701a420
Javier Constain <constainopia@gmail.com>
2014-09-23 17:08:01 -0500

    Create controller.js
    Defining its functionalities

---
+++ api/tickets/controller.js
@@ -0,0 +1,47 @@
+/** 
+ *  Ticket service
+ *  Utility Service Layer Controller
+ *  
+ *  CRUD Elements.
+ */
+
+/**
+ * Get all tickets
+ *
+ */
+
+
+/**
+ * Get tickets for a Date
+ *
+ */
+ 
+/**
+ * Get a specific ticket from the object Id
+ *
+ */
+
+/**
+ * Get a specific ticket from the user and date
+ *
+ */
+
+/**
+ * Create a ticket 
+ *
+ */
+
+/**
+ * Update a ticket
+ *
+ */
+
+/**
+ * Update the book dateTime of a ticket
+ *
+ */
+
+/**
+ * Delete a ticket
+ *
+ */ 
