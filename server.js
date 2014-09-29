dffdf58c41552bdea49fbef75b7592a3f81b6030
Javier Constain <constainopia@gmail.com>
2014-09-28 23:48:35 -0500

    updated test

--- server.js
+++ server.js
@@ -16,20 +16,34 @@ app.use(bodyParser.json('application/json'));
 /**
  * Routes
  */
-app.post('/ticket', function(req,res) {
- console.log('Post request recieved for ticket');
+app.get('/ticket', function(req, res) {
+  console.log('Get request recieved for ticket');
 
- var newTicket = req.body.ticket;
- newTicket.id = '123';
+  var ticket = {};
 
- // Build the response
- res.set('Content-Type', 'application/json');
- res.status(201);
+  // Build the response
+  res.set('Content-Type', 'application/json');
+  res.status(200);
 
- res.json({
-  ticket: newTicket
- });
+  res.json({
+   ticket: ticket
+  });
 });
+app.post('/ticket', function(req,res) {
+  console.log('Post request recieved for ticket');
+
+  var newTicket = req.body.ticket;
+  newTicket.id = '123';
+
+  // Build the response
+  res.set('Content-Type', 'application/json');
+  res.status(201);
+
+  res.json({
+   ticket: newTicket
+  });
+});
+
 
 /**
  * Start server if we're not someone else's dependency
--- test/tickets.js
+++ test/tickets.js
@@ -5,38 +5,145 @@ var host = process.env.API_TEST_HOST || api;
 request = request(host);
 
 describe('Ticket manager /ticket', function(){
+  var ticketUrl= '/ticket';
+  var data = {};
   describe('POST', function(){
     it('should create a new ticket', function (done){
-    var now = new Date();
-    var data = {
-      ticket:{
-        'username': 'Javier',
-        'booktime': now,
-        'duration': '20', 
+      var now = new Date();
+      var data = {
+            ticket:{
+              'username': 'Javier',
+              'booktime': now,
+              'duration': '20', 
 
-      }
-    };
+            }
+          };
 
-    request
-      .post('/ticket')
-      .set('Accept', 'application/json')
-      .send(data)
-      .expect(201) // Created
-      .expect('Content-Type', /application\/json/)
-      .end(function (err, res) {
-        var ticket;
+      request
+        .post(ticketUrl)
+        .set('Accept', 'application/json')
+        .send(data)
+        .expect(201) // Created
+        .expect('Content-Type', /application\/json/)
+        .end(function (err, res) {
+          var ticket;
 
-        var body = res.body;
-        expect(body).to.have.property('ticket');
-        var ticket = body.ticket;
+          var body = res.body;
+          expect(body).to.have.property('ticket');
+          var ticket = body.ticket;
 
-        expect(ticket).to.have.property('username', 'Javier');
-        expect(ticket).to.have.property('booktime', now.toISOString());
-        expect(ticket).to.have.property('duration', '20');
+          expect(ticket).to.have.property('username', 'Javier');
+          expect(ticket).to.have.property('booktime', now.toISOString());
+          expect(ticket).to.have.property('duration', '20');
 
-        done (err);
+          done (err);
 
+          });
       });
+    });
+
+
+  describe('GET', function(){
+    it ('should recieve all tickets', function(done){
+      request
+        .get(ticketUrl)
+        .set('Accept', 'application/json')
+        .send()
+        .expect(200) 
+        .expect('Content-Type', /application\/json/)
+        .end(function(err, res) {
+          
+          var body = res.body;
+          expect(body).to.have.property('ticket');
+
+          var ticket = body.ticket;
+
+          if (ticket.length > 1){
+            var oneTicket = ticket[0];
+            expect(ticket).to.have.property('username');
+            expect(ticket).to.have.property('booktime');
+            expect(ticket).to.have.property('duration');
+          }
+          done(err);
+
+        });
+    });
+  });
+
+  describe('GET', function(){
+    it ('should recieve all tickets for a day', function(done){
+      request
+        .get('/ticket/year/month/day')
+        .set('Accept', 'application/json')
+        .send()
+        .expect(200) 
+        .expect('Content-Type', /application\/json/)
+        .end(function(err, res) {
+          
+          var body = res.body;
+          expect(body).to.have.property('ticket');
+
+          var ticket = body.ticket;
+
+          expect(ticket).to.be.an('array');
+          if (ticket.length >= 1){
+            var oneTicket = ticket[0];
+            expect(ticket).to.have.property('username');
+            expect(ticket).to.have.property('booktime');
+            expect(ticket).to.have.property('duration');
+          }
+          done(err);
+
+        });
+    });
+  });
+  describe('GET', function(){
+    it('should recieve a single ticket from it\'s id', function(done){
+      request 
+        .get((ticketUrl, '/5456efgff'))
+        .set('Accept', 'application/json')
+        .send()
+        .expect(200) 
+        .expect('Content-Type', /application\/json/)
+        .end(function(err, res) {
+
+          var body = res.body;
+          expect(body).to.have.property('ticket');
+
+          expect(ticket).to.have.property('username');
+          expect(ticket).to.have.property('booktime');
+          expect(ticket).to.have.property('duration');
+
+          done(err);
+        });
+    });
+  });
+  
+  describe('PUT', function(){
+    it('should update a single ticket', function(done){
+      request
+        .put(ticketUrl)
+        .set('Accept', 'application/json')
+        .send()
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        .end(function(err, res){
+          var ticket;
+
+          var body = res.body;
+          expect(body).to.have.property('ticket');
+
+          expect(body.ticket).to.be.an('array')
+            .and.to.have.length(1);
+          ticket = body.ticket[0];
+
+          expect(ticket).to.have.property('username');
+          expect(ticket).to.have.property('booktime');
+          expect(ticket).to.have.property('duration');
+          
+          done(err);
+        });
+
     });
   });
 });
